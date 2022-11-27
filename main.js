11abc2024ee9146ba537ff4757d1762c80cba163
Devi30 <96772778+Devi30@users.noreply.github.com>
2022-11-27 16:15:03 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function startClassification(){
-    navigator.mediaDevices.getUserMedia({audio:true})
-    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/QQhSirecO/model.json',modelready())
-}
-function modelready(){
-    classifier.classify(gotresults)
-}
